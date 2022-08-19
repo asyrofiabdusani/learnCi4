6cdc65c9a758ac4fa9b2d124b30ae7d7f5392588
Asyrofi Abdusani <asyrofiabdusani@gmail.com>
2022-08-19 22:30:12 +0700

    separate the navbar code into different file

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -7,30 +7,20 @@
     <title><?= $title; ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <style>
+    .main-container {
+        width: 70%;
+        margin: 0 auto;
+    }
+    </style>
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="\pages">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="\pages\about">About</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <div class="main-container">
+        <?= $this->include('layout/navbar'); ?>
 
-    <?= $this->renderSection('content'); ?>
+        <?= $this->renderSection('content'); ?>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous">
     </script>
