56de2ee7acc11e07101d89b53e9814d678fe2435
Asyrofi Abdusani <asyrofiabdusani@gmail.com>
2022-08-21 12:54:25 +0700

    keep all input in the form when there is error

--- app/Views/pages/form_tambah.php
+++ app/Views/pages/form_tambah.php
@@ -18,25 +18,26 @@
     <div class="row mb-3">
         <label for="pengarang" class="col-sm-2 col-form-label">Pengarang</label>
         <div class="col-sm-10">
-            <input type="text" class="form-control" id="pengarang" name="pengarang">
+            <input type="text" class="form-control" id="pengarang" name="pengarang" value="<?= old('pengarang'); ?>">
         </div>
     </div>
     <div class="row mb-3">
         <label for="penerbit" class="col-sm-2 col-form-label">Penerbit</label>
         <div class="col-sm-10">
-            <input type="text" class="form-control" id="penerbit" name="penerbit">
+            <input type="text" class="form-control" id="penerbit" name="penerbit" value="<?= old('penerbit'); ?>">
         </div>
     </div>
     <div class="row mb-3">
         <label for="tahun-terbit" class="col-sm-2 col-form-label">Tahun Terbit</label>
         <div class="col-sm-10">
-            <input type="number" class="form-control" id="tahun-terbit" name="tahun-terbit">
+            <input type="number" class="form-control" id="tahun-terbit" name="tahun-terbit"
+                value="<?= old('tahun-terbit'); ?>">
         </div>
     </div>
     <div class="row mb-3">
         <label for="sampul" class="col-sm-2 col-form-label">Sampul Buku</label>
         <div class="col-sm-10">
-            <input type="text" class="form-control" id="sampul" name="sampul">
+            <input type="text" class="form-control" id="sampul" name="sampul" value="<?= old('sampul'); ?>">
         </div>
     </div>
     <button type="submit" class="btn btn-primary mt-3">Submit Data</button>
